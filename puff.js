1f18597f36d61befc1901226e4355bd98d981a66
cshawnkeech <cshawnkeech@gmail.com>
2016-07-09 00:20:47 -0600

    puff follows mouse
    puff now follows mouse. edges not yet bounded to canvas (center
    bounded).

--- puff.js
+++ puff.js
@@ -13,9 +13,13 @@ function Puff() {
   // this.alpha = 200;
   this.xTranslate = width/2 ;
   this.yTranslate = height/2;
+  this.newTargetX = width/2;
+  this.newTargetY = height/2;
   this.scaleFactor = 1;
   this.rotateFactor = 0;
   this.alphaFactor = 240;
+  this.boundingCircle = 296 * this.scaleFactor;
+  this.speedFactor = 1;
 
   this.display = function() {
 
@@ -23,8 +27,9 @@ function Puff() {
 
     push(); //begins Puff
     translate(this.xTranslate, this.yTranslate);
+    scale(-this.scaleFactor, this.scaleFactor);
     rotate(this.rotateFactor);
-    scale(this.scaleFactor, this.scaleFactor);
+
 
     translate(24 * this.scaleFactor, 22 * this.scaleFactor); //centers fish in bounding circle
 
@@ -259,175 +264,33 @@ function Puff() {
 
   }; // end this.display
 
+  this.newTarget = function() {
+    text('Hi', width/2, 100);
+
+    this.newTargetX = mouseX;
+    this.newTargetY = mouseY;
+
+  }
+
+  this.followMouse = function () {
+    text('followMouse', width/2, 100);
+    var easing = 0.05;
+    // I adapted this from
+    // http://coursescript.com/notes/interactivecomputing/animation/
+    // by John Kuiphoff
+
+    // calculate the new xpos value
+    var dx = this.newTargetX - this.xTranslate;
+    if(abs(dx) > 1) {
+      this.xTranslate += dx * easing;
+    }
+
+    // calculate the new ypos value
+    var dy = this.newTargetY - this.yTranslate;
+    if(abs(dy) > 1) {
+      this.yTranslate += dy * easing;
+    }
+
+  }
+
 };// end Puff constructor
-
-var fish1 = new Puff();
-fish1.display();
-
-
-
-
-
-// //Piggy constructor
-// function Piggy() {
-//   this.red = 235;
-//   this.green = 181;
-//   this.blue = 227;
-//   this.alpha = 240;
-//   this.xTranslate = width/3 ;
-//   this.yTranslate = height/2;
-//   this.rotateFactor = 0;
-//   this.scaleFactor = 2;
-//
-//   this.display = function() {
-//     push()
-//
-//     translate(this.xTranslate, this.yTranslate);
-//     scale(this.scaleFactor);
-//     rotate(this.rotateFactor);
-//
-//     // helper theme circle
-//     fill(this.red, this.green, this.blue , this.alpha);
-//
-//     //ears
-//     triangle(-20 ,-45, -28, -29, -12, -37); //left
-//     push();
-//     scale(-1, 1);
-//     triangle(-20 ,-45, -28, -29, -12, -37);
-//     pop();
-//
-//     //head
-//     ellipse(0, 0, 80, 80);
-//     //nose
-//     ellipse(0, 0, 20, 14);
-//     //nostrils
-//     stroke(219, 31, 197, 200);
-//     ellipse(3,0, 3, 5);
-//     ellipse(-3,0, 3, 5);
-//
-//     //eyes
-//     stroke(0);
-//     fill(56, 56, 56);
-//     ellipse(-9,-12, 6, 8);
-//     ellipse(9,-12, 6, 8);
-//
-//
-//     //mouth
-//     noFill();
-//     strokeWeight(1);
-//     arc(0, 11, 13, 10, 0, 160);
-//
-//     pop();
-//   }//end this.display
-//
-//   this.headBob = function() {
-//     this.rotateFactor = this.rotateFactor++;
-//   }// end this.headBob
-//
-//   this.playTheme = function() {
-//     var mouseDistance = dist(mouseX, mouseY, this.xTranslate, this.yTranslate);
-//     if (mouseDistance < 40 * this.scaleFactor) {
-//       helpersTheme.togglePlay();
-//     } else if (mouseDistance > 40 * this.scaleFactor && helpersTheme.isEnded() === false) {
-//       helpersTheme.stop();
-//     }
-//
-//   }//end this.playTheme
-//
-//   this.spinCheck = function() {
-//     if (helpersTheme.isPaused() === false && percent > 0) {
-//       this.rotateFactor += 0.025;
-//     } else if (helpersTheme.isPaused() || percent === 0) {
-//       if (this.rotateFactor % TWO_PI > 0) {
-//         this.rotateFactor = this.rotateFactor % TWO_PI;
-//         this.rotateFactor -= 0.025;
-//       } else if (this.rotateFactor < 0) {
-//         this.rotateFactor = 0;
-//       }
-//     }
-//   } // end this.spinCheck
-//
-//
-// } // end Piggy constructor
-//
-// //Wolf constructor
-// function Wolf() {
-//   //this.red = 235;
-//   //this.green = 181;
-//   //this.blue = 227;
-//   //this.alpha = 200;
-//   this.xTranslate = 2 * width/ 3;
-//   this.yTranslate = height/2;
-//   this.scaleFactor = 0.80;
-//   this.rotateFactor = 0;
-//
-//   this.display = function() {
-//
-//     push();//begins wolf
-//     translate(this.xTranslate, this.yTranslate);
-//     scale(this.scaleFactor);
-//     rotate(this.rotateFactor);
-//
-//     strokeWeight(2);
-//
-//     fill(176, 176, 176);
-//     quad(-100, -80, 100, -80, 20, 80, -20, 80);
-//
-//     fill(230, 225, 225);
-//     quad(-93, -75, 93, -75, 73, -36, -73, -37);
-//     quad(-20, 80, 20, 80, 14, 92, -14, 92);
-//
-//     //muzzle
-//     stroke(161, 152, 152);
-//     fill(176, 176, 176, 200);
-//     quad(-10, 0, 10, 0, 19, 80, -18, 80);
-//
-//     stroke(0, 0, 0);
-//     fill(176, 176, 176);
-//     angleMode(DEGREES);
-//     arc(0, 20, 200, 200, 218, 323);
-//     angleMode(RADIANS);
-//
-//     //eyes
-//     fill(168, 151, 151, 100);
-//     triangle(-50, -26, -10, -13, -21, 1);
-//     push(); //begins second eye
-//     scale(-1, 1);
-//     triangle(-50, -26, -10, -13, -21, 1);
-//     pop(); //ends second eye
-//
-//     pop(); //ends wolf
-//   } //ends this.display
-//
-//
-//     this.headBob = function() {
-//       this.rotateFactor = this.rotateFactor++;
-//     }// end this.headBob
-//
-//     this.playTheme = function() {
-//       var mouseDistance = dist(mouseX, mouseY, this.xTranslate, this.yTranslate);
-//       if (mouseDistance < 100 * this.scaleFactor) {
-//         wolvesTheme.togglePlay();
-//       } else if (mouseDistance > 100 * this.scaleFactor && wolvesTheme.isEnded() === false) {
-//         wolvesTheme.stop();
-//       }
-//
-//     }//end this.playTheme
-//
-//     this.spinCheck = function() {
-//       if (wolvesTheme.isPaused() === false && wolvesPercent > 0) {
-//         this.rotateFactor += 0.025;
-//       } else if (wolvesTheme.isPaused() || wolvesPercent === 0) {
-//         if (this.rotateFactor % TWO_PI > 0) {
-//           this.rotateFactor = this.rotateFactor % TWO_PI;
-//           this.rotateFactor -= 0.025;
-//         } else if (this.rotateFactor < 0) {
-//           this.rotateFactor = 0;
-//         }
-//       }
-//     } // end this.spinCheck
-//
-//
-//
-//
-// }//end wolf constructor
--- puffFishTest.js
+++ puffFishTest.js
@@ -1,87 +1,35 @@
-
-//use this for github page
-// var helpersTheme = new buzz.sound( "https://cshawnkeech.github.io/ThreeLittlePigsThemePlayer/audioExamples/HelpersTheme", {
-//     formats: [ "ogg", "mp3", "m4a", "wav" ]
-// });
-//
-// var wolvesTheme = new buzz.sound( "https://cshawnkeech.github.io/ThreeLittlePigsThemePlayer/audioExamples/WolvesTheme", {
-//     formats: ["ogg", "mp3", "m4a", "wav" ]
-// });
-
-// //use this for local atom tests
-//
-// var helpersTheme = new buzz.sound( "audioExamples/HelpersTheme", {
-//     formats: [ "ogg", "mp3", "m4a", "wav" ]
-// });
-//
-// var wolvesTheme = new buzz.sound( "audioExamples/WolvesTheme", {
-//     formats: ["ogg", "mp3", "m4a", "wav" ]
-// });
-
 var puff1;
-var puffPercent;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  //createCanvas (460, 320);
 
   puff1 = new Puff();
   puff1.scaleFactor = 0.5;
 
-
 } // end setup
 
 function draw() {
+
   background(19, 148, 247);
 
   push();
   fill(245);
-  text(windowWidth, 30, 30);
-  text(windowHeight, 30, 50);
-  //text(pig.rotateFactor, 30, 70);
+  //text(puff1.distanceToMouse(), 30, 30);
+  text("targetX " + puff1.newTargetX, width/2, 50);
+  text("targetY " + puff1.newTargetY, width/2, 70);
   //text(pig.rotateFactor % TWO_PI, 30, 90)
   pop();
 
-
+  puff1.followMouse();
   puff1.display();
-
-  //puff1.rotateFactor++;
-
-
-  // percent = helpersTheme.getPercent();
-  // wolvesPercent = wolvesTheme.getPercent();
-  // background(45);
-  //
-  // // //testing text
-
-  // // //end testing text
-  //
-  // pig.display();
-  // pig.spinCheck();
-  //
-  // wolf1.display();
-  // wolf1.spinCheck();
 
 } // end draw
 
+function mouseMoved() {
+puff1.newTarget();
+} // end mouseMoved
+
+
 function mousePressed() {
-  // pig.playTheme();
-  // wolf1.playTheme();
+
 } //end mousePressed
-
-
-//for reference, from buzz example
-/*
-var mySound = new buzz.sound( "/sounds/myfile", {
-    formats: [ "ogg", "mp3", "aac" ]
-});
-
-mySound.play()
-    .fadeIn()
-    .loop()
-    .bind( "timeupdate", function() {
-       var timer = buzz.toTimer( this.getTime() );
-       document.getElementById( "timer" ).innerHTML = timer;
-    });
-
-*/
